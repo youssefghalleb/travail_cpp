3d0e6ab501da0a296dc3086023d978726a4733e2
youssefghalleb <youssef.ghalleb@universite-paris-saclay.fr>
2026-05-12 19:57:17 +0200

    Update point.cpp

--- src/point.cpp
+++ src/point.cpp
@@ -1,3 +1,14 @@
 #include "point.hpp"
+#include <cmath>
 
-Point::Point(double x_val, double y_val) : x(x_val), y(y_val) {}
+Point::Point(double x_val, double y_val) {
+    x = x_val;
+    y = y_val;
+}
+
+double Point::distance(Point B) {
+    return std::sqrt(
+        (B.x - x) * (B.x - x) +
+        (B.y - y) * (B.y - y)
+    );
+}
